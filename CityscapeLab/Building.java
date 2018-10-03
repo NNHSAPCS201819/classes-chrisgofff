731f3878ec273cb28a52ee86f958b84f3851f94e
chrisgofff <42477081+chrisgofff@users.noreply.github.com>
2018-10-03 13:35:57 -0500

    started city scaepe lab

---
+++ CityscapeLab/Building.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+public class Building
+{
+    private double yTop;
+    private double xLeft;
+    
+    public Building(int x, int y)
+    {
+        xLeft = x;
+        yTop = y;
+    }
+    
+    public void draw(Graphics2D g2)
+    {
+        
+    }
+}
